74125183a9a7b68659ea15fb6385237b2787d5b8
singh-harsh-03 <harshsingh14012003@gmail.com>
2024-03-28 22:37:41 +0530

    Initial commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+      Welcome to TheCocktailDB, your ultimate destination for all things cocktails! Whether you're a seasoned mixologist or a curious beginner, our website is your go-to resource for discovering, creating, and enjoying the perfect cocktail.
+      </p>
+      <p>
+      At TheCocktailDB, we're passionate about sharing our love for mixology and helping you unleash your inner bartender. Dive into our extensive database of cocktail recipes, featuring everything from timeless classics to trendy creations. With detailed instructions, ingredient lists, and helpful tips, crafting your favorite drinks has never been easier.
+      </p>
+      <p>
+      Whether you're planning a cocktail party, searching for the perfect drink to impress your friends, or simply looking to expand your cocktail repertoire, TheCocktailDB has you covered. Cheers to endless possibilities and unforgettable libations—let the cocktail journey begin!
+      </p>
+    </section>
+  );
+}
